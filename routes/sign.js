00fe7dcae40aef9f16d19f0812c04dc250eb505b
mye0ngs00 <ourendpoint@gmail.com>
2020-01-07 09:12:05 +0900

    exception update

--- routes/sign.js
+++ routes/sign.js
@@ -56,32 +56,36 @@ router.get('/out', (req, res)=>{
 
 /* sign up */
 router.get('/up', (req, res)=>{
-    res.render('sign_up');
+    if( !req.session.username ) res.render('sign_up');
+    else res.send('잘못된 요청입니다.');
 });
 
 router.post('/up', (req, res)=>{
     let id = req.body.id;
     let pw = req.body.password;
-
-    let query = 'SELECT * FROM users WHERE id=?';
-    req.app.conn.query(query, [id], (err, result)=>{
-        if(err) console.error(err);
-        // validate.
-        if(!result[0]){
-            let salt = crypto.randomBytes(16).toString('hex');
-            query = 'INSERT INTO users (id, password, salt) VALUES (?, ?, ?)';
-            // success.
-            crypto.pbkdf2(pw, salt, 100000, 64, 'sha512', (err2, keyStream)=>{
-                if( err2 ) console.error(err2);
-                req.app.conn.query(query, [id, keyStream.toString('hex'), salt], (err3, result)=>{
-                    if(err3) console.error(err3); //throw err;
-                    else res.redirect('/sign/in');
+    // exception just web.
+    if( !id || !pw ) res.render('sign_up', {msg: "정보를 확인해주세요."});
+    else{
+        let query = 'SELECT * FROM users WHERE id=?';
+        req.app.conn.query(query, [id], (err, result)=>{
+            if(err) console.error(err);
+            // validate.
+            if(!result[0]){
+                let salt = crypto.randomBytes(16).toString('hex');
+                query = 'INSERT INTO users (id, password, salt) VALUES (?, ?, ?)';
+                // success.
+                crypto.pbkdf2(pw, salt, 100000, 64, 'sha512', (err2, keyStream)=>{
+                    if( err2 ) console.error(err2);
+                    req.app.conn.query(query, [id, keyStream.toString('hex'), salt], (err3, result)=>{
+                        if(err3) console.error(err3); //throw err;
+                        else res.redirect('/sign/in');
+                    });
                 });
-            });
-        }
-        // failure.
-        else res.render('sign_up', {msg: "이미 존재하는 아이디 입니다."});
-    });
+            }
+            // failure.
+            else res.render('sign_up', {msg: "이미 존재하는 아이디 입니다."});
+        });
+    }
 });
 
 module.exports = router;
